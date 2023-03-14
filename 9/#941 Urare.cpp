10ed52f07e7c8dec15876e80afde0caa52d30a05
gabrielvlad <gabriel.vlad@outlook.dk>
2023-03-14 14:07:37 +0100

    Algo about wishing Happy Holidays
    Created by Gabe on 14/03/2023
    I did the output in Romanian,Spanish,English and Danish.

---
+++ 9/#941 Urare.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+Created by Gabe on 14/03/2023.
+
+Around Christmas we are all or become children again.
+
+requirement
+Write a program that displays a wish for your loved ones on the screen!
+
+Input data
+The program does not read any value.
+
+Output data
+The program will display a wish for loved ones on the screen.
+Restrictions and clarifications
+any wish for your loved ones will be considered correct!!
+
+Example
+Output
+Happy celebrations!
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Craciun Fericit!/Feliz navidad!/Marry Christmas!/Glædelig jul!";
+    return 0;
+}
