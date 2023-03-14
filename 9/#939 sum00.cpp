9b62b37a2d499e1808a1076e4a324eabc2c4f7d5
gabrielvlad <gabriel.vlad@outlook.dk>
2023-03-14 13:58:38 +0100

    sum of 2 given natural numbers
    14.03.2023

---
+++ 9/#939 sum00.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+Created by Gabe on 14/03/2023.
+
+requirement
+They give 2 natural numbers. Calculate the sum of those 2 given numbers.
+
+ Input data
+The program reads from the keyboard 2 natural numbers.
+
+ Output data
+The program will display the sum of the two numbers on the screen.
+Restrictions and clarifications
+the two numbers will be smaller than 1.000.000.000
+
+ Example:
+Input
+12 23
+Output
+35
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<x+y;
+    return 0;
+}
