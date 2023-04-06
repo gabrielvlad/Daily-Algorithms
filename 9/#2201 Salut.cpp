6cbd181f9262fb0670d902f56f225ea63839d537
gabrielvlad <gabriel.vlad@outlook.dk>
2023-04-06 22:32:56 +0200

    Update for Algo about handshakes using nested loops
    Created by Gabe on 06/04/2023.
    Update details:
    This is the second iteration because the loops solution was not using a long long int and apparently the given one or more sets of input data, consistent with problem-specific constraints were validating it as not a complete solution
    
    Logic part:
    The formula calculates the number of distinct pairs that can be formed from a set of n elements.
    For example, if n is 4, the formula would give us: 4*(4-1)/2 = 6, which means there are 6 distinct pairs that can be formed from the set of 4 elements, such as (1,2), (1,3), (1,4), (2,3), (2,4), and (3,4).
    
    Note:
    Both iterations are considered complete solutions and give max points now when evaluated.

--- 9/#2201 Salut.cpp
+++ 9/#2201 Salut.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-Created by Gabe on 05/04/2023.
+Created by Gabe on 06/04/2023.
 
 Requirement
 In a group there are n friends. When they meet they greet each other, each shaking hands with everyone else. How many handshakes take place?
@@ -23,26 +23,45 @@ Output
 Explanation
 If the three friends are A B C, then the following handshakes occur: (A,B), (A,C) and (B,C).
 *******************************************************************************/
-
-#include <iostream>
+//First iteration using nested loops
+/*
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
     int n;
-    cin >> n;
+    cin>>n;
 
-    int handshakeCount = 0;
-    for(int i = 0; i < n; i++)
+    long long handshakeCount=0;
+    for(long long i=0; i<n; i++)
     {
-        for(int j = i+1; j < n; j++)
+        for(long long j=i+1; j<n; j++)
         {
             handshakeCount++;
         }
     }
 
-    cout << handshakeCount << endl;
+    cout<<handshakeCount<<endl;
 
+    return 0;
+}
+*/
+
+//This is the second iteration because the loops solution was not using a long long int and apparently the given one or more sets of input data, consistent with problem-specific constraints were validating it as not a complete solution.
+/*
+ * The formula calculates the number of distinct pairs that can be formed from a set of n elements.
+ * For example, if n is 4, the formula would give us: 4*(4-1)/2 = 6, which means there are 6 distinct pairs that can be formed from the set of 4 elements, such as (1,2), (1,3), (1,4), (2,3), (2,4), and (3,4).
+ */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    long long hand_shakes=n*(n-1)/2;//since we are interested in distinct numbers we divide by 2
+    cout<<hand_shakes;
     return 0;
 }
